7febb661b86c7cb3e25585142bdd7bff491ac79f
David Törnros <david@dxgruppen.se>
2024-02-23 09:45:10 +0100

    added missing imports

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import "./App.css";
 import Countdown from "./components/Countdown";
 import Description from "./components/Description";
+import Answers from "./components/Answers";
+import Picture from "./components/Picture";
 
 function App() {
 	const [score, setScore] = useState(0);
