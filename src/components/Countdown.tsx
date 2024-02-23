43be4e0b2b20dde8ff688374ecaba5e71b1ed1c3
David Törnros <david@dxgruppen.se>
2024-02-23 09:41:45 +0100

    added components

---
+++ src/components/Countdown.tsx
@@ -0,0 +1,9 @@
+interface PictureProps {
+	countdown: number;
+}
+
+const Countdown = (props: PictureProps) => {
+	const { countdown } = props;
+};
+
+export default Countdown;
---
+++ src/components/Picture.tsx
@@ -0,0 +1,8 @@
+interface PictureProps {
+	countdown: number;
+}
+const Picture = (props: PictureProps) => {
+	const { countdown } = props;
+};
+
+export default Picture;
